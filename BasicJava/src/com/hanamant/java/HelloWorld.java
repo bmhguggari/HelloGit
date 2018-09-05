76b327ab5db7c55d83431df2d96505bc6269b310
bmhguggari <bmh.guggari@yahoo.in>
2018-09-05 22:55:10 +0530

    Reverted GitRepo changes

--- BasicJava/src/com/hanamant/java/HelloWorld.java
+++ BasicJava/src/com/hanamant/java/HelloWorld.java
@@ -3,6 +3,5 @@ package com.hanamant.java;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world to java programming");
-		System.out.println("This file has been edited in Git Repo");
 	}
 }
